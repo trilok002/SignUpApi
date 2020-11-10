e012fd90713c5c42c490517aa65b4d0c7a37b2be
trilok002 <61768477+trilok002@users.noreply.github.com>
2020-11-10 17:10:49 +0530

    msg

---
+++ Blogger/Blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Blog.models import *
+
+# Register your models here.
+admin.site.register(Category),
+admin.site.register(Post),
+admin.site.register(likecomment),
+admin.site.register(userdetail)
