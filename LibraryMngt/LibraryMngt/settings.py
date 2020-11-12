a050cc8fe207801a0454056b9a04a03f97221330
trilok002 <61768477+trilok002@users.noreply.github.com>
2020-11-12 09:33:45 +0530

    libr

--- LibraryMngt/LibraryMngt/settings.py
+++ LibraryMngt/LibraryMngt/settings.py
@@ -127,7 +127,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
 EMAIL_HOST_USER = "trilokkumrawat002@gmail.com"
-EMAIL_HOST_PASSWORD = '7024855041'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
